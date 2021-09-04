822865b3986015b556f03a56f669c3a7ad98f763
蒸发杰作 <1160564525@qq.com>
2021-09-04 11:55:12 +0800

    刷新

--- _site/search_json.js
+++ _site/search_json.js
@@ -1,22 +1,10 @@
 window.ydoc_plugin_search_json = {
-  "documents": [
+  "百科全书": [
     {
       "title": "嘿，伙计",
       "content": "大城市的日子总是那么叫人难堪。这日子可什么时候是个头啊。你看了看面前众多高耸的大厦，但是，这些大厦，哪里有你的立足之地呢？你就这样漫无目的的走着，看着，你目光几乎快要变得朦胧了...突然，你包里叮铃铃想起了电话...--电话那边--喂喂...儿子呀...你快回来，爸爸不行啦...你快回来，别在大城市打拼啦...家里好几亩地等着你种呢...\n你有些慌张与哽咽。赶紧定了一张今天下午的机票，马上准备飞回家...",
       "url": "\\documents\\index.html",
       "children": []
-    },
-    {
-      "title": "Introduction",
-      "content": "Introduction",
-      "url": "\\documents\\intro.html",
-      "children": [
-        {
-          "title": "title",
-          "url": "\\documents\\intro.html#title",
-          "content": "titlecontent"
-        }
-      ]
     },
     {
       "title": "env",
---
+++ index.md
@@ -0,0 +1,3 @@
+# 文档地址
+
+https://zgarry.github.io/myFarm-doc/_site/documents/index.html
